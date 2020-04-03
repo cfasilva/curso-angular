a67388bb28e082cd16517adac933ae5816d9fc78
cfasilva <cleberfasilva@gmail.com>
2020-04-03 16:26:55 -0300

    Add primeiro-projeto

---
+++ primeiro-projeto/src/app/cursos/cursos.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cursos',
+  templateUrl: './cursos.component.html',
+  styleUrls: ['./cursos.component.css']
+})
+export class CursosComponent implements OnInit {
+
+  nomePortal: string
+  cursos: string[] = ['Java', 'Angular']
+
+  constructor() {
+    this.nomePortal = 'http://loiane.training'
+
+    for (let i = 0; i < this.cursos.length; i++) {
+      const curso = this.cursos[i];
+    }
+   }
+
+  ngOnInit(): void {
+  }
+
+}
